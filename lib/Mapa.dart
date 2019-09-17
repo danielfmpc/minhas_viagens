1d095be7a92029bfb15c4118e0e4da9c478a0bc5
Daniel Fernando <daniel_fmpc@hotmail.com.br>
2019-09-17 16:58:47 -0300

    Adicionando Geolocator, exibindo localização do usuario e mostrando a rua marcado

--- lib/Mapa.dart
+++ lib/Mapa.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class AbrirMapa extends StatefulWidget {
@@ -10,30 +11,70 @@ class AbrirMapa extends StatefulWidget {
 class _AbrirMapaState extends State<AbrirMapa> {
   Completer<GoogleMapController> _controller = Completer();
   Set<Marker> _marcadores = {};
+  CameraPosition _cameraPosition =  CameraPosition(
+  target: LatLng(-23.562436, -46.655005),
+  zoom: 18
+  );
 
   _onMapCreated(GoogleMapController controller) {
     _controller.complete(controller);
   }
 
-  _exibirMarcador( LatLng latLng){
-//    Set<Marker> marcadoresLocal = {};
-    Marker marcadoEscolhido = Marker(
-      markerId: MarkerId("marcador-${latLng.latitude} - ${latLng.longitude}"),
-      position: LatLng(latLng.latitude, latLng.longitude),
-      infoWindow: InfoWindow(
-        title: "Marcado",
-      ),
-      icon: BitmapDescriptor.defaultMarkerWithHue(
-          BitmapDescriptor.hueRed
-      ),
-    );
-    // Mostra apenas um marcador
-//    marcadoresLocal.add(marcadoEscolhido);
-    setState(() {
-//      _marcadores = marcadoresLocal;
-      _marcadores.add(marcadoEscolhido);
+  _exibirMarcador( LatLng latLng) async {
+    Set<Marker> marcadoresLocal = {};
+    List<Placemark> listaEnderecos = await Geolocator().placemarkFromCoordinates(latLng.latitude, latLng.longitude);
 
+    if(listaEnderecos != null && listaEnderecos.length>0){
+      Placemark endereco = listaEnderecos[0];
+      String rua = endereco.thoroughfare;
+      Marker marcadoEscolhido = Marker(
+        markerId: MarkerId("marcador-${latLng.latitude} - ${latLng.longitude}"),
+        position: LatLng(latLng.latitude, latLng.longitude),
+        infoWindow: InfoWindow(
+          title: rua,
+        ),
+        icon: BitmapDescriptor.defaultMarkerWithHue(
+            BitmapDescriptor.hueRed
+        ),
+      );
+      // Mostra apenas um marcador
+    marcadoresLocal.add(marcadoEscolhido);
+      setState(() {
+      _marcadores = marcadoresLocal;
+//        _marcadores.add(marcadoEscolhido);
+
+      });
+    }
+  }
+
+  _movimentarCamera()async{
+    GoogleMapController googleMapController = await _controller.future;
+    googleMapController.animateCamera(
+        CameraUpdate.newCameraPosition(
+          _cameraPosition
+        )
+    );
+  }
+
+  _adicionarListenerLocalizacao(){
+    var geolocato = Geolocator();
+    var locationOptions = LocationOptions(accuracy: LocationAccuracy.high);
+    geolocato.getPositionStream(locationOptions).listen((Position postion){
+      setState(() {
+        _cameraPosition = CameraPosition(
+          target: LatLng(postion.latitude, postion.longitude),
+          zoom: 18
+        );
+        _movimentarCamera();
+      });
     });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _adicionarListenerLocalizacao();
   }
 
   @override
@@ -45,10 +86,7 @@ class _AbrirMapaState extends State<AbrirMapa> {
       body: Container(
         child: GoogleMap(
           mapType: MapType.normal,
-          initialCameraPosition: CameraPosition(
-              target: LatLng(-23.562436, -46.655005),
-              zoom: 18
-          ),
+          initialCameraPosition: _cameraPosition,
           onMapCreated: _onMapCreated,
           onLongPress: _exibirMarcador,
           markers: _marcadores,
