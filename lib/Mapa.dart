4f44f9709635a05fbb36fbec346977391d14a9f5
Daniel Fernando <daniel_fmpc@hotmail.com.br>
2019-09-17 16:28:59 -0300

    Criando marcadores

--- lib/Mapa.dart
+++ lib/Mapa.dart
@@ -9,13 +9,31 @@ class AbrirMapa extends StatefulWidget {
 
 class _AbrirMapaState extends State<AbrirMapa> {
   Completer<GoogleMapController> _controller = Completer();
+  Set<Marker> _marcadores = {};
 
   _onMapCreated(GoogleMapController controller) {
     _controller.complete(controller);
   }
 
   _exibirMarcador( LatLng latLng){
-    
+//    Set<Marker> marcadoresLocal = {};
+    Marker marcadoEscolhido = Marker(
+      markerId: MarkerId("marcador-${latLng.latitude} - ${latLng.longitude}"),
+      position: LatLng(latLng.latitude, latLng.longitude),
+      infoWindow: InfoWindow(
+        title: "Marcado",
+      ),
+      icon: BitmapDescriptor.defaultMarkerWithHue(
+          BitmapDescriptor.hueRed
+      ),
+    );
+    // Mostra apenas um marcador
+//    marcadoresLocal.add(marcadoEscolhido);
+    setState(() {
+//      _marcadores = marcadoresLocal;
+      _marcadores.add(marcadoEscolhido);
+
+    });
   }
 
   @override
@@ -33,6 +51,7 @@ class _AbrirMapaState extends State<AbrirMapa> {
           ),
           onMapCreated: _onMapCreated,
           onLongPress: _exibirMarcador,
+          markers: _marcadores,
         ),
       ),
     );
